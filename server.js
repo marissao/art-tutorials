556b6c9c2785856defc91162a8ab0f491afe9a77
marissao <50351548+marissao@users.noreply.github.com>
2021-08-28 10:02:25 -1000

    Updated login/registration forms, added handlers

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+const handlers = require('./lib/handlers');
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main', // Specifies default layout
 }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
 app.use(express.static(__dirname + '/static'));
-console.log(__dirname);
 
-app.get('/', (req, res) => res.render('guest-home'));
+app.get('/', handlers.home);
 
-app.get('/login', (req, res) => res.render('login'));
+app.get('/register', handlers.register);
+app.post('/register/process', handlers.registerProcess);
 
-app.get('/register', (req, res) => res.render('register'));
+app.get('/login', handlers.login);
+app.post('/login/process', handlers.loginProcess);
 
-app.get('/create-course', (req, res) => res.render('create-course'));
+app.get('/create-course', handlers.createCourse);
 
-app.get('/edit-course', (req, res) => res.render('edit-course'));
+app.get('/edit-course', handlers.editCourse);
 
-app.get('/course-details', (req, res) => res.render('course-details'));
+app.get('/course-details', handlers.courseDetails);
 
 // Custom 404 Page
 app.use((req, res) => {
